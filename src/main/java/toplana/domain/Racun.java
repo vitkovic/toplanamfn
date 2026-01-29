dd36d958db1e2b04568158884f6574f479d8441a
AMD <AMD@DESKTOP-2REM9EA>
2026-01-29 12:35:54 +0100

    zaduzenje manje od 0.5

--- src/main/java/toplana/domain/Racun.java
+++ src/main/java/toplana/domain/Racun.java
@@ -295,8 +295,7 @@ public class Racun implements Serializable {
     	
 		}
     	//ako je saldo negativan ili jednak 0
-    	if((this.ukupnoZaduzenje.compareTo(new BigDecimal("0.")) <= 0 && this.stan.getTipPotrosaca().getTip() != 5) || 
-    			(this.ukupnoZaduzenje.compareTo(new BigDecimal("0.")) <= 0.5 && this.stan.getTipPotrosaca().getTip() != 5)) {
+    	if((this.ukupnoZaduzenje.compareTo(new BigDecimal("0.")) <= 0.5 && this.stan.getTipPotrosaca().getTip() != 5)) {
     		
     		this.popust = nacrtRacuna.getPopust();
     		
